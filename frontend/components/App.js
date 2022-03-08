a51cd22b30e90327a8a1298d07eb922fbd92428d
Andre Blaze <kuraiokaimblaze@gmail.com>
2022-03-08 14:33:41 -0700

    app.js_set up getTodos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState = {
+  successMessage: '',
+  errorMessage: '',
+  todos: [],
+  form: {
+    nameInput: "",
+    completed: false,
+  }, 
+}
 export default class App extends React.Component {
+  state = initialState;
+
+  componentDidMount(){
+    this.getTodos()
+  }
+
+  getTodos = () => {
+    axios.get(URL)
+    .then(res=>{
+      this.setState({
+        ...this.state, 
+        todos: res.data.data,
+        successMessage: res.data.message,
+      })
+      console.log(this.state.todos)
+      console.log(this.state.successMessage)
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        errorMessage: err,
+      })
+    })
+  }
+  
   render() {
     return null
   }
