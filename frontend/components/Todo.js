82cadfaa817d5fe5b737870523eb9469bfd84962
Andre Blaze <kuraiokaimblaze@gmail.com>
2022-03-08 15:24:17 -0700

    Todo.js_updated with info_showing todos

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
-    return null
+    const { complete, info } = this.props
+    console.log(info)
+    return (
+      <div className='todo'>
+        <h3
+          onClick={evt => complete(info.id)}
+          style = {{ textDecoration: info.completed ? 'line-through' : '' }}
+        >
+          {info.name}
+        </h3>
+      </div>
+    )
   }
 }
