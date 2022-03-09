8766a4bb2b2a642174419182b6e337c1200d030d
Andre Blaze <kuraiokaimblaze@gmail.com>
2022-03-08 17:08:06 -0700

    Form.js_set up & showing

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,7 +1,33 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  onSubmit = evt => {
+    evt.preventDefault();
+    this.props.onSubmit();
+  }
+  onChange = evt => {
+    const { value, id } = evt.target
+    this.props.onChange(id, value)
+  }
+
   render() {
-    return 
+    const { values, clear } = this.props
+    console.log(values)
+    
+    return (
+      <div className='input-form'>
+        <form onSubmit={this.onSubmit}>
+          <input 
+            value={values.nameInput}
+            onChange={this.onChange}
+            type="text"
+            id="nameInput"
+            placeholder='Add Todo'
+          />
+          <input type="submit" />
+        </form>
+        <button onClick={() => clear()}>Clear Completed Todos</button>
+      </div>
+    )
   }
 }
